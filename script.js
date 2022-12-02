9dbad1b25fef59ea4e3503b093a9813152aacbfc
Rohit Kumar <rohitkumarmehra58@gmail.com>
2022-12-02 23:35:29 +0530

    initial

---
+++ script.js
@@ -0,0 +1,33 @@
+// checking the candidate Eligible to vote or not 
+
+/* const validate = () => {
+let age = 17;
+if(age === 18){
+console.log('Make Your VoterID');
+}else if(age > 18){
+console.log('Eligible to Vote');
+}else {
+console.log('Not-Eligible to Vote');
+}
+return;
+}
+validate(); */
+
+
+
+
+// 2.  FizzBuzz using Arrow function in javascript
+
+
+/* const fizzbuzz = () => {
+let num = 9;
+if(num % 3 === 0 && num % 5 === 0){
+console.log('FizzBuzz');
+}else if(num % 3 === 0){
+console.log('Buzz');
+}else{
+console.log('Fizz')
+}
+return;
+}
+fizzbuzz(); */
